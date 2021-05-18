2627980bc51688baa7e2ce07d9ec3a1e7f1b9abb
Muhammad Jawwad <kjawwad973@gmail.com>
2021-05-18 20:53:31 +0500

    Non Successful to download at home

---
+++ api/shortTest.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = 4000;
+const cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// const sql = require("msnodesqlv8");
+
+// const connectionString = "server=202.143.127.178;Database=Biztekmain;User ID=erpuser;Password=ghs@wasnazskr@786@;Driver={SQL Server Native Client 11.0}";
+// const query = "SELECT userid FROM users";
+
+// sql.query(connectionString, query, (err, rows) => {
+//     console.log('reach')
+//     console.log(rows);
+//     console.log(err)
+// });
+
+const sql = require('mssql')
+
+const config = {
+    user: 'erpuser',
+    password: 'ghs@wasnazskr@786@',
+    server: '202.143.127.178', 
+    port : 1433,
+    database: 'biztekmain',
+    options: {
+        encrypt: true // Use this if you're on Windows Azure
+    }
+}
+var conn = new sql.ConnectionPool(config);
+
+ conn.connect()
+ // Successfull connection
+ .then(function () {
+
+   // Create request instance, passing in connection instance
+   var req = new sql.Request(conn);
+
+   // Call mssql's query method passing in params
+   req.query("select 1 as number")
+   .then(function (recordset) {
+     console.dir(recordset);
+     conn.close();
+   })
+   // Handle sql statement execution errors
+   .catch(function (err) {
+     console.log(err);
+     conn.close();
+   })
+ })
+ // Handle connection errors
+ .catch(function (err) {
+   console.log(err);
+   conn.close();
+ });
+
+
+
+app.listen(PORT, function(){
+  console.log('Server is running on Port:',PORT);
+});
+
